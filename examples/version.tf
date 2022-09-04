4bc22b7007d67301551d34c65a7f9e9e52f90dc5
Sean Sundberg <seansund@us.ibm.com>
2022-09-03 22:11:53 -0500

    Updates provider handling in test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- examples/version.tf
+++ examples/version.tf
@@ -1,9 +1,17 @@
 terraform {
   required_providers {
     gitops = {
-      #source  = "cloud-native-toolkit/gitops"
-      source  = "terraform.local/local/gitops"
+      source  = "cloud-native-toolkit/gitops"
       version = "0.0.0"
     }
+  }
+}
+provider_installation {
+  filesystem_mirror {
+    path    = "/tmp/terraform/providers"
+    include = ["registry.terraform.io/cloud-native-toolkit/*"]
+  }
+  direct {
+    exclude = ["registry.terraform.io/cloud-native-toolkit/*"]
   }
 }
