cb6ba1cc1c159770987bdb28107a0b337aa731b2
Sean Sundberg <seansund@us.ibm.com>
2022-09-06 16:03:03 -0500

    Randomize test repo name to avoid conflicts
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- examples/stage2-namespace.tf
+++ examples/stage2-namespace.tf
@@ -2,11 +2,11 @@
 module cntk_namespace {
   source = "github.com/cloud-native-toolkit/terraform-gitops-namespace.git?ref=v1.12.2"
 
-  name = "cntk"
-  server_name = module.gitops.server_name
-  config = module.gitops.gitops_config
-  credentials = module.gitops.git_credentials
-  ci = false
+  name            = "cntk"
+  server_name     = module.gitops.server_name
+  gitops_config   = module.gitops.gitops_config
+  git_credentials = module.gitops.git_credentials
+  ci              = false
 }
 
 resource local_file namespace {
