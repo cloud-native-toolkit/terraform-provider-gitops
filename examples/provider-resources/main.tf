3939f60d77e76fad2829564dc3be5bea62648fef
Sean Sundberg <seansund@us.ibm.com>
2022-09-17 13:25:40 -0500

    Simplifies test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- examples/provider-resources/main.tf
+++ examples/provider-resources/main.tf
@@ -5,34 +5,34 @@ locals {
   ci = true
 }
 
-resource gitops_namespace ns {
-
-  name = var.namespace
-  create_operator_group = local.create_operator_group
-  argocd_namespace = local.argocd_namespace
-  dev_namespace = local.ci
-  server_name = var.server_name
-  branch = local.application_branch
-  config = yamlencode(var.gitops_config)
-  credentials = yamlencode(var.git_credentials)
-}
-
-resource gitops_service_account test {
-  name = "test-rbac"
-  namespace = gitops_namespace.ns.name
-  server_name = var.server_name
-  branch = local.application_branch
-  config = yamlencode(var.gitops_config)
-  credentials = yamlencode(var.git_credentials)
-  roles {
-    name = "cluster-admin"
-  }
-  rules {
-    api_groups = [""]
-    resources = ["configmaps","secrets"]
-    verbs = ["*"]
-  }
-}
+#resource gitops_namespace ns {
+#
+#  name = var.namespace
+#  create_operator_group = local.create_operator_group
+#  argocd_namespace = local.argocd_namespace
+#  dev_namespace = local.ci
+#  server_name = var.server_name
+#  branch = local.application_branch
+#  config = yamlencode(var.gitops_config)
+#  credentials = yamlencode(var.git_credentials)
+#}
+#
+#resource gitops_service_account test {
+#  name = "test-rbac"
+#  namespace = gitops_namespace.ns.name
+#  server_name = var.server_name
+#  branch = local.application_branch
+#  config = yamlencode(var.gitops_config)
+#  credentials = yamlencode(var.git_credentials)
+#  roles {
+#    name = "cluster-admin"
+#  }
+#  rules {
+#    api_groups = [""]
+#    resources = ["configmaps","secrets"]
+#    verbs = ["*"]
+#  }
+#}
 
 resource gitops_seal_secrets no_annotation {
   source_dir = "${path.module}/secrets"
@@ -40,11 +40,11 @@ resource gitops_seal_secrets no_annotation {
   kubeseal_cert = var.kubeseal_cert
   tmp_dir = "${path.cwd}/.tmp/no-annotation"
 }
-
-resource gitops_seal_secrets annotation {
-  source_dir = "${path.module}/secrets"
-  dest_dir   = "${path.cwd}/annotation"
-  kubeseal_cert = var.kubeseal_cert
-  tmp_dir = "${path.cwd}/.tmp/annotation"
-  annotations = ["test=value"]
-}
+#
+#resource gitops_seal_secrets annotation {
+#  source_dir = "${path.module}/secrets"
+#  dest_dir   = "${path.cwd}/annotation"
+#  kubeseal_cert = var.kubeseal_cert
+#  tmp_dir = "${path.cwd}/.tmp/annotation"
+#  annotations = ["test=value"]
+#}
