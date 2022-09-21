4ab83d7c71e902932c2ee742ef288ef521ecc86a
Sean Sundberg <seansund@us.ibm.com>
2022-09-21 14:21:04 -0500

    Fixes provider config
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- examples/provider.tf
+++ examples/provider.tf
@@ -5,11 +5,11 @@ data clis_check clis {
 provider "gitops" {
   username = var.git_username
   token = var.git_token
-  bin_dir  = data.clis_check.clis
+  bin_dir  = data.clis_check.clis.bin_dir
 }
 
 resource local_file bin_dir {
   filename = "${path.cwd}/.bin_dir"
 
-  content = data.clis_check.clis
+  content = data.clis_check.clis.bin_dir
 }
